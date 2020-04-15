9116c70a96392c11fb80b8bc52294284f9671dd4
Traff-dev <61308331+Traff-dev@users.noreply.github.com>
2020-04-15 12:16:18 +0300

    fix misstake

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -52,7 +52,7 @@ static const Checkpoints::CCheckpointData data = {
     1542152086, // * UNIX timestamp of last checkpoint block
     0,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
-    0        // * estimated number of transactions per day after checkpoint
+    500       // * estimated number of transactions per day after checkpoint
 };
 static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
     boost::assign::map_list_of
